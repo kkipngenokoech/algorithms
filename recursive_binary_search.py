0a9b344fabfdd9387d74c6b6069633022b3897a4
KKipngeno_koech <84391547+kkipngeno-koech@users.noreply.github.com>
2022-01-13 16:06:10 +0300

    Add files via upload
    binary search and recursive binary search algorithms

---
+++ recursive_binary_search.py
@@ -0,0 +1,17 @@
+def recursive_binary_search(list,target):
+    if len(list)== 0:
+        return False
+    else:
+        midpoint=len(list)//2
+        if list[midpoint]==target:
+            return True
+        else:
+            if list[midpoint]<target:
+                return recursive_binary_search(list[midpoint+1:],target)
+            else:
+                return recursive_binary_search(list[:midpoint-1],target)
+def verify(result):
+    print("target found: ",result)
+numbers=[1,2,3,4,5,6,7,8,9,10]
+result=recursive_binary_search(numbers,9)
+verify(result)
